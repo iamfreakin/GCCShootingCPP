8a34d838e5b17a71bb1d942b4720f14c92ce8463
iamfreakin <qjaqjaqja1877@gmail.com>
2026-05-07 15:20:53 +0900

    feat: EnemyFactory 적 생성 로직 구현 - 타이머에 따른 주기적 EnemyActor 스폰

---
+++ Source/GCC_TopView/Public/EnemyFactory.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemyFactory.generated.h"
+
+UCLASS()
+class GCC_TOPVIEW_API AEnemyFactory : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AEnemyFactory();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class AEnemyFactory> enemy;
+	
+	UPROPERTY(EditAnywhere)
+	float dlayTime = 2.0f;
+	
+private:
+	float currntTime = 0.0f;	
+};
